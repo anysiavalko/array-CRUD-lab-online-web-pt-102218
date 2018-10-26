6cc86b1a2ed272a13b12fb621316ce44f059a85c
Anysia Valko <anysiavalko@gmail.com>
2018-10-26 18:35:50 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,11 +13,14 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["I", "am", "really", "learning"]
+  element = "wow"
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["I", "am", "really", "learning"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
