5953fad1e3d495e01cec7297681eb618bd132e85
Anysia Valko <anysiavalko@gmail.com>
2018-10-26 18:36:51 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,12 +19,12 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["I", "am", "really", "learning"]
+  array = ["I", "am", "really", "learning", "arrays!"]
   array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
 end
 
 def retrieve_element_from_index(array, index_number)
